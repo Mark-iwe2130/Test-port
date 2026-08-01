db4c803228f8d3ae404eedc6db875935b8a7b94c
Mark-iwe2130 <bagdauletaltynbekov163@gmail.com>
2026-08-01 21:44:03 +0500

    Add files via upload

---
+++ assets/shared/stages/bedroom.lua
@@ -0,0 +1,12 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('stageback', 'bedroomgrey', -470, -350);
+	addLuaSprite('stageback', false);
+	scaleObject('stageback', 1.4, 1.4);
+
+makeLuaSprite('tablething', 'bedblock', 850, 520);
+	addLuaSprite('tablething', true);
+	scaleObject('tablething', 1.4, 1.4);
+
+end
+
